1168f06eb5de74b7e6449b35a88e12d821b9c2af
nunes-willi <nuneswillroberto@gmail.com>
2026-08-20 10:58:30 -0300

    Usuário válido

--- financeiro/conta.py
+++ financeiro/conta.py
@@ -5,7 +5,9 @@ class Conta:
         self.saldo = saldo
         
     def abrir_conta(self) -> bool:
-        if self.idade < 18:
+        if self.nome == "":
+            raise ValueError("Precisa de nome do Usuário da conta")
+        elif self.idade < 18:
             raise ValueError("Precisa ser maior de idade para abrir conta")
         elif self.saldo <= 0:
             raise ValueError ("É preciso ter um SALDO maior que 0(zero)!!")
